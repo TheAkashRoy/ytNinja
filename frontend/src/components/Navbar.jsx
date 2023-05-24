47428aaa298e201f9fff3dfd166f219eb88c7b9b
TheAkashRoy <9akash.roy@gmail.com>
2023-05-24 09:25:22 +0530

    vercel setup

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid d-flex justify-content-center">
+          <span className="navbar-brand mb-0 h1">ytNinja</span>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
